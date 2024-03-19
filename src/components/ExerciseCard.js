fbdf204b49a69fe574ed2e0efe5e23e4c805c57f
David Gonnzalez <davidgonzalez_1999@yahoo.com>
2024-03-18 21:26:40 -0700

    getting data to appear as a gif when searcged for in search exerxices text box

---
+++ src/components/ExerciseCard.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import { Button, Stack, Typography } from "@mui/material"
+const ExerciseCard = ({ exercise }) => {
+  return (
+    <Link className="exercise-card" to={`/exercises/${exercise.id}`}>
+      <img src={exercise.gifUrl} alt={exercise.name} Loading="lazy" />
+      <Stack direction="row">
+        <Button
+          sx={{
+            ml: "21px",
+            color: "fff",
+            background: "#ffa9a9",
+            fontSie: "14px",
+            borderRadius: "30px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.bodyPart}
+        </Button>
+        <Button
+          sx={{
+            ml: "21px",
+            color: "fff",
+            background: "#ffa9a9",
+            fontSie: "14px",
+            borderRadius: "30px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.target}
+        </Button>
+      </Stack>
+    </Link>
+  )
+}
+export default ExerciseCard
--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -2,17 +2,25 @@ import React, {useEffect, useState} from "react";
 import { Pagination } from "@mui/material";
 import {Box, Stack, Typography } from "@mui/material"
 import { exerciseOptions, fetchData } from "../utils/fetchData";
-
+import ExerciseCard from "../components/ExerciseCard"
 const Exercises = ({exercises, setExercises, bodyPart}) => {
+  console.log(exercises)
   return (
-    <div>
       <Box id="exercises" sx={{mt: {
         lg:"100px"
-      }}} mt="50px" p="20px"></Box>
-      <Typography variant="h4">
+      }}} mt="50px" p="20px">
+      <Typography variant="h3" mb="46px">
         Showing Results
       </Typography>
-    </div>
+      <Stack direction="row" sx={{ gap: {lg:"100px", xs: "50px"}}} flexWrap="wrap" justifyContent="center">
+        {exercises.map((
+          exercise,index) => (
+            <ExerciseCard key={index} exercise={exercise}/>
+          )
+        )}
+      </Stack>
+
+      </Box>
   )
 }
 
